a7755a123bdd7915e42f42c7688185e34863538c
khamaruayan <169046571+khamaruayan@users.noreply.github.com>
2024-06-04 18:07:19 +0530

    Add files via upload

---
+++ Promise.js
@@ -0,0 +1,33 @@
+/*let p=new Promise((resolve, reject)=>{
+    let a=1+2
+    if(a==2){
+    resolve('Success')
+    }
+    else{
+        reject('Failed')
+    }
+})
+
+p.then((message)=>{
+    console.log('This is in the then'+ message)
+}).catch((message)=>{
+    console.log('This is in the catch'+ message)
+})*/
+
+const recordVideoOne= new Promise((resolve, reject)=>{
+    resolve('Video 1 Recorded')
+})
+const recordVideoTwo= new Promise((resolve, reject)=>{
+    resolve('Video 2 Recorded')
+})
+const recordVideoThree= new Promise((resolve, reject)=>{
+    resolve('Video 3 Recorded')
+})
+
+Promise.all([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+]).then((messages)=>{
+    console.log(messages)
+})
