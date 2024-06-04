a6a3e3e518b6cffd59d8275993e466542f782ee2
khamaruayan <169046571+khamaruayan@users.noreply.github.com>
2024-06-04 18:06:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs=require("node:fs/promise")
+console.log("First");
+fs.readFile("file.txt", "utf-8")
+.then((data)=> console.log(data))
+.catch((error)=> console.log(error));
+console.log("Second");
